f3acf77316545bc5a009431486321a0c2e7dcbba
Tom Fuertes <tomfuertes@gmail.com>
2014-08-03 18:13:58 -0500

    large refactor

--- fstimizely.js
+++ fstimizely.js
@@ -2,36 +2,115 @@
 
 var UPLOAD = process.argv[2] === 'up';
 
-var optimizely = require('./lib/optimizely');
+var Optimizely = require('./lib/optimizely');
 
 var fs = require('fs');
 var slug = require('slug');
-var conf = require('rc')('fstimizely', {});
-var prompt = require('sync-prompt').prompt;
-var yesNo = require('yes-no').parse;
-var jsdiff = require('diff');
 var git = require('git-promise');
 var gitUtil = require('git-promise/util');
 require('colors');
 
 /**
- * globals
+ * Token API for multiple projects
+ * cat ~/.fstimizelyrc # {'tokens': {'$name':'$token'}}
+ * cat ./.fstimizelyrc # {'$name':'$experiment_id'}
+ *
+ * note: overarchitected for prep to move to account level sync
  */
-var
-  log = function () {
-    if (arguments[0] !== undefined)
-      console.log(arguments[0]);
-  },
-  onError = function (err) {
-    log('ERROR :( / Docs: https://github.com/clearhead/fstimizely');
-    log(err);
-  };
+var API_TOKEN, EXPERIMENT_ID; // ewww
+(function () {
+  var conf = require('rc')('fstimizely', {});
+  if (!conf.tokens) throw new Error('.fstimizelyrc requires tokens object');
+  Object.keys(conf).forEach(function (key) {
+    if (['_', 'config', 'tokens'].indexOf(key) === -1) {
+      API_TOKEN = conf.tokens[key];
+      EXPERIMENT_ID = conf[key];
+    }
+  });
+  if (!API_TOKEN) throw new Error('.fstimizelyrc api_token missing');
+  if (!EXPERIMENT_ID) throw new Error('.fstimizelyrc experiment_id missing');
+})();
+var optimizely = new Optimizely(API_TOKEN);
 
 /**
- * compares text / prints diff
+ * Avoid losing non-commited changes by failing unless
+ * you're on a clean git tree
  */
-var isDifferent = function (name, start, end) {
-  log(('\nDIFF: ' + name + ' ->').blue);
+git('status --porcelain', gitUtil.extractStatus)
+  .then(function (status) {
+    var err;
+    ['modified', 'added', 'deleted', 'renamed', 'copied'].forEach(function (b) {
+      if (status.workingTree[b].length) {
+        err = 'dirty git tree - please stash/commit first';
+        console.error(err.red);
+        throw err;
+      }
+    });
+    return true;
+  }).then(function () { // Yay now we can run!
+    // globaljs and css
+    optimizely.getExperiment(EXPERIMENT_ID)
+      .then(function (experiment) {
+        writeOrUpload(experiment, 'experiments/' + experiment.id, 'global.js', 'custom_js');
+        writeOrUpload(experiment, 'experiments/' + experiment.id, 'global.css', 'custom_css');
+      });
+    // variation js
+    optimizely.getVariations(EXPERIMENT_ID)
+      .then(function (variations) {
+        variations.forEach(function (variation) {
+          writeOrUpload(variation, 'variations/' + variation.id,
+            slug(variation.description).toLowerCase() + '.js', 'js_component');
+        });
+      });
+  });
+
+
+
+/*jshint latedef:false*/
+
+/**
+ * Write or Upload files based on upload/download state
+ * @param  {object} obj      object in question
+ * @param  {string} url      url endpoint
+ * @param  {string} fileName filename in question
+ * @param  {string} key      key on object to check
+ */
+function writeOrUpload(obj, url, fileName, key) {
+  fs.readFile(fileName, function (err, data) {
+    // if in upload mode, confirm then
+    //  modify put {`key`: `fsText`} to `url`
+    if (UPLOAD) {
+      if (err) return;
+      data = data.toString(); // idk
+      if (isDifferent(fileName, obj[key], data)) {
+        if (getAnswer('Upload diff to ' + fileName)) { // sync / needs to be nested
+          var stingy = {};
+          stingy[key] = data;
+          optimizely.put(url, stingy).then(function () {
+            console.log('Uploaded to: https://www.optimizely.com/edit?experiment_id=' + EXPERIMENT_ID);
+          });
+        }
+      }
+    } else {
+      // Print diff of obj[key] and fileText & write file
+      if (err) data = '';
+      if (isDifferent(fileName, data.toString(), obj[key])) {
+        fs.writeFile(fileName, obj[key]);
+      }
+    }
+  });
+}
+
+/**
+ * Prints a diff
+ * @param  {string}  name  name of diff
+ * @param  {string}  start base to diff
+ * @param  {string}  end   base to compare
+ * @return {Boolean}       start != end
+ */
+function isDifferent(name, start, end) {
+  var jsdiff = require('diff');
+  console.log(('\nDIFF: ' + name + ' ->').blue);
   var diff = jsdiff.diffLines(start, end);
   var lastLine; // force newline print on lastLine
   diff.forEach(function (part) {
@@ -42,104 +121,18 @@ var isDifferent = function (name, start, end) {
   });
   if (!lastLine.value.match(/\n$/)) console.log('\n');
   return start != end; // jshint ignore:line
-};
+}
 
 /**
- * Token API for multiple projects
- * cat ~/.fstimizelyrc # {'tokens': {'$name':'$token'}}
- * cat ./.fstimizelyrc # {'$name':'$experiment_id'}
+ * prompt for an answer
+ * @param  {string} q question to ask [y/N]?
+ * @return {Boolean}   answer
  */
-if (!conf.tokens) throw new Error('.fstimizelyrc requires tokens object');
-Object.keys(conf).forEach(function (key) {
-  if (['_', 'config', 'tokens'].indexOf(key) === -1) {
-    conf.api_token = conf.tokens[key];
-    conf.experiment_id = conf[key];
-  }
-});
-if (!conf.api_token) throw new Error('.fstimizelyrc api_token missing');
-if (!conf.experiment_id) throw new Error('.fstimizelyrc experiment_id missing');
-
-optimizely.init(conf.api_token);
-
-var getAnswer = function (q) {
+function getAnswer(q) {
+  var prompt = require('sync-prompt').prompt;
+  var yesNo = require('yes-no').parse;
   var a = prompt(q + '? [y/N]: ');
   if (a === '') a = false;
   else a = yesNo(a);
   return a;
-};
-
-var conditionalWriteFile = function (name, txt) {
-  fs.readFile(name, function (err, data) {
-    if (err) data = '';
-    if (isDifferent(name, data.toString(), txt)) {
-      fs.writeFile(name, txt);
-    }
-  });
-};
-
-function getExperiments(eid) {
-  optimizely.getExperiments(eid);
-    .then(function (experiment) {
-      function processGlobal(fileName, key) {
-        fs.readFile(fileName, function (err, data) {
-          if (err) return;
-          if (isDifferent(fileName, experiment[key], data.toString())) {
-            if (getAnswer('Upload diff to ' + fileName)) {
-              var x = {};
-              x[key] = data.toString();
-              optimizely.put('experiments/' + experiment.id, x, log).then(function () {
-                log('Uploaded to: https://www.optimizely.com/edit?experiment_id=' + eid);
-              });
-            }
-          }
-        });
-      }
-
-      if (UPLOAD) {
-        processGlobal('global.js', 'custom_js');
-        processGlobal('global.css', 'custom_css');
-      } else {
-        conditionalWriteFile('global.js', experiment.custom_js);
-        conditionalWriteFile('global.css', experiment.custom_css);
-      }
-    }, onError);
 }
-
-function getVariations(eid) {
-  optimizely.get('experiments/' + eid + '/variations/')
-    .then(function (variations) {
-      if (UPLOAD) {
-        variations.forEach(function (variation) {
-          var name = slug(variation.description).toLowerCase() + '.js';
-          fs.readFile(name, function (err, data) {
-            if (err) return;
-            if (isDifferent(name, variation.js_component, data.toString())) {
-              if (getAnswer('Upload diff to ' + name)) {
-                variation.js_component = data.toString();
-                optimizely.put('variations/' + variation.id, variation).then(function () {
-                  log('Uploaded to: https://www.optimizely.com/edit?experiment_id=' + eid);
-                });
-              }
-            }
-          });
-        });
-      } else {
-        variations.forEach(function (variation) {
-          conditionalWriteFile(slug(variation.description).toLowerCase() + '.js', variation.js_component);
-        });
-      }
-    }, onError);
-}
-
-git('status --porcelain', gitUtil.extractStatus).then(function (status) {
-  var err;
-  ['modified', 'added', 'deleted', 'renamed', 'copied'].forEach(function (b) {
-    if (status.workingTree[b].length) {
-      err = 'dirty git tree - please stash/commit first';
-      console.error(err.red);
-      throw err;
-    }
-  });
-  getExperiments(conf.experiment_id);
-  getVariations(conf.experiment_id);
-});
